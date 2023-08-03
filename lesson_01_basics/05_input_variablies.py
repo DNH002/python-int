56e7b859f91fe1a097524fb0b074be97345c3d0e
DNH002 <dabhod23@gmail.com>
2023-08-03 09:49:46 +0300

    Adding HomrWork_01

---
+++ lesson_01_basics/05_input_variablies.py
@@ -0,0 +1,37 @@
+### Practice_03_Square_area
+
+# num = float(input("Enter number"))
+# print(num ** 2)
+
+### Practice_04_sum
+
+# num_01, num_02 = map(int,input("Enter 2 numbers ").split())
+# print(num_01)
+# print(num_02)
+# print(num_01 + num_02)
+
+### Practice_05
+
+# num_01, num_02 = map(float,input("Enter 2 numbers: ").split())
+# print(num_01 * num_02)
+# print(2 * (num_01 + num_02))
+
+### Practice_06_Fahrenheit
+
+# num = float(input("Enter number: "))
+# print((num - 32) * 5 / 9)
+
+#### Practice_07
+
+# a, b, c, d = map(float,input("Enter four numbers: ").split())
+# print((a + b + c + d)/4)
+
+### Prctice_08
+
+# a, b, c, d, e = map(int,input("Enter five numbers: ").split())
+# print(max(a,b,c,d,e))
+
+### Prctice_09
+
+# a = int(input("Enter number: "))
+# print(a-1,a,a+1, sep='<')
---
+++ lesson_01_basics/06_Home_Work_input.py
@@ -0,0 +1,62 @@
+### HW_01
+
+# a, b = map(int,input("Enter 2 numbers ").split())
+# print(abs(a))
+# print(abs(b))
+# print(abs(a) + abs(b))
+
+### HW_02
+
+# a, b = map(float,input("Enter 2 numbers ").split())
+# print(a)
+# print(b)
+# print(abs(a) + abs(b))
+
+### HW_03
+
+# a = float(input("Enter number: "))
+# print(round(a,2))
+# print(round(a,3))
+
+### HW_05
+
+# a, b, c = map(int,input("Enter 3 numbers ").split())
+# print(a)
+# print(b)
+# print(c)
+#
+# # pencil = 3
+# # pen = 5
+# # marker = 12
+#
+# print((a * 3) + (b * 5) + (c * 12))
+
+
+### HW_06
+
+# a = int(input("Enter Weight in [gram]: "))
+# b = (a/1000)
+# print(round(b,0))
+
+### HW_07
+
+a = int(input("Enter number: "))
+print(a)
+
+### HW_04
+
+# a, b, c = map(int,input("Enter 3 numbers: ").split())
+# print(a)
+# print(b)
+# print(c)
+# op_1 = (a * b + c)
+# op_2 = (a + b * c)
+# op_3 = ((a + b) * c)
+# op_4 = (a * b * c)
+#
+# max_op = max(op_1, op_2,op_3, op_4)
+#
+# print(max_op)
+
+
+
